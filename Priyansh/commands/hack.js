9c230adb2ddd693035d44d28ad8319647dbad8d2
PRINCE MEGHWANSI <princemeghwansi1@gmail.com>
2025-02-09 09:55:25 +0530

    Update hack.js

--- Priyansh/commands/hack.js
+++ Priyansh/commands/hack.js
@@ -56,7 +56,7 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
   
   var background = [
 
-    "https://i.imgur.com/VQXViKI.png"
+    "https://i.ibb.co/vxTjWRB8/image.png"
 ];
   var rd = background[Math.floor(Math.random() * background.length)];
   
